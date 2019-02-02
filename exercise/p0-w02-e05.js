71421ee899db1d958b91989fef77a8e1dfe89be2
emzhofb <ikhdamuhammad@gmail.com>
2019-02-02 20:35:51 +0700

    upload exercise bermain dengan string

---
+++ exercise/p0-w02-e05.js
@@ -0,0 +1,108 @@
+/*
+String
+Soal latihan string ini memiliki 4 nomor. Simpan di 1 file yang sama!
+
+1. Let's Form a Sentence
+Problem
+Pada tugas ini kamu diminta untuk melakukan penambahan string menggunakan simbol +. Disediakan variable word. Tambahkan nilai word satu per satu dengan nilai variable lain untuk membentuk sebuah kalimat. Jangan lupa menambahkan spasi di setiap kata, dan tampilkan di console hasil penggabungannya! Kamu tidak perlu membuat variable baru!
+
+Skeleton Code
+*/
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+console.log(word, second, third, fourth, fifth, sixth, seventh)
+/*
+Output
+JavaScript is awesome and I love it!
+2. Index Accessing - 1 by 1
+Problem
+Pada tugas ini kamu diminta untuk "memecah" sebuah kalimat dan menampilkan setiap kata didalamnya. Untuk soal nomor ini, gunakan akses satu per satu karakter dari string untuk mengambil setiap huruf dalam kata. Terasa manual? Tidak apa-apa, kita coba ini dulu untuk saat ini.
+
+Hints
+Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata tersebut!
+
+Skeleton Code
+*/
+var word2 = 'wow JavaScript is so cool';
+var exampleFirstWord = word2[0] + word2[1] + word2[2];
+var secondWord = word2[4] + word2[5] + word2[6] + word2[7] + word2[8] + word2[9] + word2[10] + word2[11] + word2[12] + word2[13]; // do your own!
+var thirdWord = word2[15] + word2[16]; // do your own!
+var fourthWord = word2[18] + word2[19]; // do your own!
+var fifthWord = word2[21] + word2[22] + word2[23] + word2[24]; // do your own!
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+/*
+Output
+First Word: wow
+Second Word: JavaScript
+Third Word: is
+Fourth Word: so
+Fifth Word: cool
+3. Breaking Sentence (Again) using Substring
+Problem
+Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+Skeleton Code
+*/
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word3.substring(0, 3);
+var secondWord3 = word3.substring(4, 14); // do your own!
+var thirdWord3 = word3.substring(15, 17); // do your own!
+var fourthWord3 = word3.substring(18, 20); // do your own!
+var fifthWord3 = word3.substring(21); // do your own!
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
+/*
+Output
+First Word: wow
+Second Word: JavaScript
+Third Word: is
+Fourth Word: so
+Fifth Word: cool
+4. Breaking Sentence (yet Again) and Count Each Length
+Problem
+Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+Skeleton Code
+Buatlah variable-variable baru untuk menyimpan panjang string, dan ubah console dibawah untuk menampilkan nya.
+*/
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word4.substring(0, 3);
+var secondWord4 = word4.substring(4, 14); // do your own!
+var thirdWord4 = word4.substring(15, 17); // do your own!
+var fourthWord4 = word4.substring(18, 20); // do your own!
+var fifthWord4 = word4.substring(21); // do your own!
+
+var firstWordLength = exampleFirstWord4.length;
+var secondWordLength = secondWord4.length;
+var thirdWordLength = thirdWord4.length;
+var fourthWordLength = fourthWord4.length;
+var fifthWordLength = fifthWord4.length;
+// create new variables around here
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWordLength);
+/*
+Output
+First Word: wow, with length: 3
+Second Word: JavaScript, with length: 10
+Third Word: is, with length: 2
+Fourth Word: so, with length: 2
+Fifth Word: cool, with length: 4
+*/
