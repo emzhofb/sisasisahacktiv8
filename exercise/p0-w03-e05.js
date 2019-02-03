40f61429aa1f64923d8952cad06e2f899d0ec243
emzhofb <ikhdamuhammad@gmail.com>
2019-02-03 16:18:29 +0700

    upload exercise palindrome

---
+++ exercise/p0-w03-e05.js
@@ -0,0 +1,22 @@
+/*
+Logic Challenge - Palindrome
+Problem
+Diberikan sebuah function palindrome(kata) yang menerima satu parameter. Function tersebut akan mengembalikan nilai true jika kata merupakan palindrome, dan false jika bukan. Kata palindrome adalah sebuah kata yang jika dibalik, tetap sama. Contoh, 'katak' dibalik tetaplah 'katak'.
+
+Code
+*/
+function palindrome(kata) {
+  // you can only write your code here!
+  let atak = '';
+  for (let i = kata.length-1; i >= 0; i--) {
+    atak += kata[i];
+  }
+  return atak === kata ? true : false;
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
