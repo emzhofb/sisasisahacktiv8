1fffed9e362499c4616abafb7e646f80b3854630
emzhofb <ikhdamuhammad@gmail.com>
2019-02-10 19:16:58 +0700

    upload exercise deret geometri

--- exercise/p0-w03-e12.js
+++ exercise/p0-w03-e12.js
@@ -1,39 +1,25 @@
 /*
-Logic Challenge - Target Terdekat
+Logic Challenge - Tentukan Deret Geometri
 Problem
-Diberikan sebuah function targetTerdekat(arr) yang menerima satu parameter berupa array yang terdiri dari karakter. Function akan me-return jarak spasi antar karakter 'o' dengan karakter 'x' yang terdekat. Contoh, jika arr adalah ['x', ' ', 'o', ' ', ' ', 'x'], maka jarak terdekat dari 'o' ke 'x' adalah 2. Jika tidak ditemukan 'x' sama sekali, function akan me-return nilai 0.
+Diberikan sebuah function tentukanDeretGeometri(arr) yang menerima satu parameter berupa array yang terdiri dari angka. Function tersebut akan mengembalikan true jika array dari parameter tersebut merupakan deret geometri. Deret geometri adalah sebuah deret dimana perbedaan setiap angka di deret tersebut konsisten secara perkalian. Contoh, [1, 3, 9, 27, 81] adalah deret aritmatika dengan pertambahan nilai sebesar pengalian 3, dan [1, 3, 9, 27, 48] bukanlah deret aritmatika karena tidak perbedaan selisih antar angka yang tidak konsisten secara perkalian (27 * 3 bukanlah 48!).
 
 Code
 */
-function targetTerdekat(arr) {
+function tentukanDeretGeometri(arr) {
   // you can only write your code here!
-  let oPosition = 0;
-  let xTarget = [];
+  const interval = arr[arr.length-1] / arr[arr.length-2];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 'o') {
-        oPosition = i;
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[j] === 'x') {
-                xTarget.push(Math.abs(j-oPosition));
-            }
-        }
+    if (arr[i+1] / arr[i] === interval) {
+        return true;
+    } else {
+        return false;
     }
   }
-  let result = xTarget[0];
-  for (let i = 0; i < xTarget.length; i++) {
-    if (result > xTarget[i]) {
-        result = xTarget[i];
-    }
-  }
-  if (xTarget[0] === undefined) {
-    return 0;
-  }
-  return result;
 }
 
 // TEST CASES
-console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
-console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
-console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
-console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
-console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
+console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
+console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
+console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
+console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
