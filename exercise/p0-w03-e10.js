20e4765558c0c06aa4702d39fb703b0e9206273d
emzhofb <ikhdamuhammad@gmail.com>
2019-02-10 19:16:15 +0700

    upload exercise perkalian unik

--- exercise/p0-w03-e10.js
+++ exercise/p0-w03-e10.js
@@ -1,25 +1,26 @@
 /*
-Logic Challenge - Tentukan Deret Aritmatika
+Logic Challenge - Perkalian Unik
 Problem
-Diberikan sebuah function tentukanDeretAritmatika(arr) yang menerima satu parameter berupa array yang terdiri dari angka. Function tersebut akan mengembalikan true jika array dari parameter tersebut merupakan deret aritmatika. Deret aritmatika adalah sebuah deret dimana perbedaan setiap angka di deret tersebut konsisten. Contoh, [2, 4, 6, 8] adalah deret aritmatika dengan pertambahan nilai sebesar 2, dan [2, 4, 6, 9] bukanlah deret aritmatika karena tidak perbedaan selisih antar angka yang tidak konsisten.
+Diberikan sebuah function perkalianUnik(arr) yang menerima satu parameter berupa array yang berisikan angka. Function akan me-return array baru yang mengandung angka yang setiap nilainya merupakan hasil kali angka lain yang bukan angka itu sendiri. Contoh, jika arr adalah [1, 2, 3, 4, 5], maka function akan mereturn [120, 60, 40, 30, 24], karena 120 adalah 2 * 3 * 4 * 5, 60 adalah 1 * 3 * 4 * 5, 40 adalah 1 * 2 * 4 * 5, dan seterusnya.
 
 Code
 */
-function tentukanDeretAritmatika(arr) {
+function perkalianUnik(arr) {
   // you can only write your code here!
-  const interval = arr[arr.length-1] - arr[arr.length-2];
+  let result = [];
+  let temp = 1;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i+1] - arr[i] === interval) {
-        return true;
-    } else {
-        return false;
-    }
+    temp *= arr[i];
   }
+  for (let i = 0; i < arr.length; i++) {
+    result.push(temp / arr[i]);
+  }
+  return result;
 }
 
 // TEST CASES
-console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
-console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
-console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
-console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
-console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
+console.log(perkalianUnik([2, 4, 6])); // [24, 12, 8]
+console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
+console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
+console.log(perkalianUnik([1, 3, 3, 1])); // [9, 3, 3, 9]
+console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
