16400609742174d100a3592d48ede8ea0466e892
emzhofb <ikhdamuhammad@gmail.com>
2019-02-10 19:15:49 +0700

    upload exercise mencari mean

--- exercise/p0-w03-e09.js
+++ exercise/p0-w03-e09.js
@@ -1,26 +1,23 @@
 /*
-Logic Challenge - Perkalian Unik
+Logic Challenge - Mencari Mean
 Problem
-Diberikan sebuah function perkalianUnik(arr) yang menerima satu parameter berupa array yang berisikan angka. Function akan me-return array baru yang mengandung angka yang setiap nilainya merupakan hasil kali angka lain yang bukan angka itu sendiri. Contoh, jika arr adalah [1, 2, 3, 4, 5], maka function akan mereturn [120, 60, 40, 30, 24], karena 120 adalah 2 * 3 * 4 * 5, 60 adalah 1 * 3 * 4 * 5, 40 adalah 1 * 2 * 4 * 5, dan seterusnya.
+Diberikan sebuah function cariMean(arr) yang menerima sebuah array angka. Function akan me-return mean dari array atau deret angka tersebut. Mean adalah angka rata-rata dari deret bilangan tersebut. Contoh, mean atau rata-rata dari [1, 2, 3, 4, 5] adalah 3. Kita perlu kemudian melakukan pembulatan angka dari hasil mean yang didapatkan.
 
 Code
 */
-function perkalianUnik(arr) {
+function cariMean(arr) {
   // you can only write your code here!
-  let result = [];
-  let temp = 1;
+  let sumNum = 0;
   for (let i = 0; i < arr.length; i++) {
-    temp *= arr[i];
+    sumNum += arr[i];
   }
-  for (let i = 0; i < arr.length; i++) {
-    result.push(temp / arr[i]);
-  }
-  return result;
+  const mean = Math.round(sumNum / arr.length);
+  return mean;
 }
 
 // TEST CASES
-console.log(perkalianUnik([2, 4, 6])); // [24, 12, 8]
-console.log(perkalianUnik([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
-console.log(perkalianUnik([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
-console.log(perkalianUnik([1, 3, 3, 1])); // [9, 3, 3, 9]
-console.log(perkalianUnik([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
+console.log(cariMean([1, 2, 3, 4, 5])); // 3
+console.log(cariMean([3, 5, 7, 5, 3])); // 5
+console.log(cariMean([6, 5, 4, 7, 3])); // 5
+console.log(cariMean([1, 3, 3])); // 2
+console.log(cariMean([7, 7, 7, 7, 7])); // 7
