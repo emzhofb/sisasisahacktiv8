bf2ab9d518b12079050b0a6858e701bcc4fe4632
emzhofb <ikhdamuhammad@gmail.com>
2019-02-12 22:37:43 +0700

    nyerah

---
+++ latihan/ady-sl-array-002.js
@@ -0,0 +1,55 @@
+/*
+    ======================
+    String to Multidimensi
+    ======================
+    [INSTRUKSI]
+    diberikan sebuah string yang jumlah characternya bisa di akar pangkat
+    (4, 9, 16, 25 , ... ) 
+    ubahlah karakter - karakter itu menjadi array multidimensi
+    dimana hasil akar pangkat jumlah karakternya menjadi row dan col
+    [EXAMPLE]
+    stringToMultidimensi("0120194124213712")
+    jumlah char : 16 
+    row dan col = akat 16 
+    row dan col = 4
+    output: 
+    [
+         1 2 3 4 
+        [0,1,2,0], 1 
+        [1,9,4,1], 2 
+        [2,4,2,1], 3
+        [3,7,1,2]  4
+    ]
+ */
+
+function stringToMultidimensi(str) {
+    // your code here
+    if (Math.pow(str.length, 0.5) % 2 !== 0) {
+        console.log('char count invalid');
+    } else {
+        let result = [];
+        for (let i = 0; i < Math.pow(str.length, 0.5); i++) {
+            let temp = [];
+            for (let j = 0; j < Math.pow(str.length, 0.5); j++) {
+                temp.push(str[j]);
+            }
+            result.push(temp);
+        }
+        console.log(result);
+    }
+}
+
+stringToMultidimensi("0120194124213712")
+/*
+   [
+       [0,1,2,0],
+       [1,9,4,1],
+       [2,4,2,1],
+       [3,7,1,2]
+   ]
+*/
+
+stringToMultidimensi("01201941242137127")
+/*
+   // char count invalid
+*/
