df324e4ded43668e1b1afe0d5bea61f4ae3a0bf8
Ikhda Muhammad Wildani <ikhdamuhammad@gmail.com>
2019-04-02 19:17:55 +0700

    upload latihan loopingConditionExercise 001

---
+++ latihan/ady-sl-lce-001.js
@@ -0,0 +1,45 @@
+/*
+    ============
+    Generate Bar 
+    ============
+    Buatlah Program untuk mengenerate Bar ( persegi Panjang ) berdasarkan 
+    - p => panjang 
+    - t => tinggi 
+*/
+
+function generateBar(p, t) {
+  // your code here
+  if (p < t) console.log("panjang harus lebih besar dari tinggi");
+  else {
+    let result = "";
+    for (let i = 0; i < t; i++) {
+      let temp = "o";
+      for (let j = 0; j < p; j++) {
+        if (j === p - 1) temp += "o";
+        else if (i === 0) temp += "o";
+        else if (i === t - 1) temp += "o";
+        else temp += " ";
+      }
+      console.log(temp);
+    }
+
+    console.log(result);
+  }
+}
+
+generateBar(4, 3); //
+/*
+    oooo
+    o  o
+    oooo
+*/
+
+generateBar(10, 4); //
+/*
+    oooooooooo
+    o        o
+    o        o
+    oooooooooo
+*/
+
+generateBar(4, 5); // panjang harus lebih besar daripada tinggi
