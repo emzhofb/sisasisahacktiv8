dc6e671ad56fcb907edae801f5c07c18b08df849
Ikhda Muhammad Wildani <ikhdamuhammad@gmail.com>
2019-04-02 22:37:40 +0700

    upload latihan builtInFunction Array.f

--- workspace.js
+++ workspace.js
@@ -1,18 +1,1 @@
-var array1 = [5, 12, 8, 130, 44];
-
-function isLargeNumber(element) {
-  return element > 13;
-}
-
-console.log(array1.findIndex(isLargeNumber));
-// expected output: 3
-
-function myFindIndex(array, number) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > number) {
-      console.log(i);
-      break;
-    }
-  }
-}
-myFindIndex(array1, 13);
+a;
