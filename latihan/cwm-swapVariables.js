60e226d537ef47d4276814db7d118f3c22661424
Ikhda Muhammad Wildani <ikhdamuhammad@gmail.com>
2019-04-02 13:12:30 +0700

    upload latihan cwm

---
+++ latihan/cwm-swapVariables.js
@@ -0,0 +1,14 @@
+let a = "red";
+let b = "blue";
+
+// Swap a and b
+
+let c = a;
+// console.log(c);
+a = b;
+// console.log(a);
+b = c;
+// console.log(b);
+
+console.log(a); // blue
+console.log(b); // red
