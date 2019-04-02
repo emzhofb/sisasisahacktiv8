0305dc02941545d6244c72a1412c9bc729259125
Ikhda Muhammad Wildani <ikhdamuhammad@gmail.com>
2019-04-02 19:20:06 +0700

    upload latihan loopingConditionExercise 001

--- latihan/ady-sl-lce-001.js
+++ latihan/ady-sl-lce-001.js
@@ -13,9 +13,10 @@ function generateBar(p, t) {
   else {
     let result = "";
     for (let i = 0; i < t; i++) {
-      let temp = "o";
+      let temp = "";
       for (let j = 0; j < p; j++) {
-        if (j === p - 1) temp += "o";
+        if (j === 0) temp += "o";
+        else if (j === p - 1) temp += "o";
         else if (i === 0) temp += "o";
         else if (i === t - 1) temp += "o";
         else temp += " ";
@@ -29,17 +30,17 @@ function generateBar(p, t) {
 
 generateBar(4, 3); //
 /*
-    oooo
-    o  o
-    oooo
-*/
+        oooo
+        o  o
+        oooo
+    */
 
 generateBar(10, 4); //
 /*
-    oooooooooo
-    o        o
-    o        o
-    oooooooooo
-*/
+        oooooooooo
+        o        o
+        o        o
+        oooooooooo
+    */
 
 generateBar(4, 5); // panjang harus lebih besar daripada tinggi
