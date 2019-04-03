a5ce9fe9f617e2a3f5171215faae041c1b6c5800
ikhda <ikhdamuhammad@gmail.com>
2019-04-04 00:47:02 +0700

    upload latihan legend 001 - not done yet

---
+++ latihan/ady-sl-legend-addAll.js
@@ -0,0 +1,29 @@
+/*
+  diberikan sebuah array angka dimana , angka - angka tersebut adalah 
+  increment dari angka sebelumnya sebesar 1 . 
+  tugas kalian adalah menjumlahkan seluruh angka tersebut dengan syarat: 
+  - Tidak boleh memakai perulangan 
+  - Tidak boleh memakai function magic apapun kecuali .length pada array
+  - Tidak boleh memakai rekursif 
+*/
+
+function addAll(array) {
+  // your code here
+  let first = array[0];
+  let increment = array[1] - array[0];
+  let all = array.length;
+  let result = first * all + (first + increment);
+  return result;
+}
+
+console.log(addAll([1, 2, 3, 4, 5]));
+// 15
+
+console.log(addAll([5, 6, 7, 8, 9, 10]));
+// 45
+
+console.log(addAll([2, 3, 4, 5, 6]));
+// 20
+
+// console.log(addAll([8, 7, 6, 5, 4, 3, 2]));
+// 35
